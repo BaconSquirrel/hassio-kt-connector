5c439aeb4b77f6c30949b3228138a0af95fc9b1b
BaconSquirrel <PaddiFries@gmail.com>
2021-02-21 19:18:47 +0100

    feat(eventtypes): add new event types for toggles and media player control

--- src/main/kotlin/de/baconsquirrel/hassiokt/connector/websocket/JsonObjectConvertorExtensions.kt
+++ src/main/kotlin/de/baconsquirrel/hassiokt/connector/websocket/JsonObjectConvertorExtensions.kt
@@ -3,26 +3,7 @@ package de.baconsquirrel.hassiokt.connector.websocket
 import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.json
-import de.baconsquirrel.hassiokt.connector.general.BooleanTurnOffMessage
-import de.baconsquirrel.hassiokt.connector.general.BooleanTurnOnMessage
-import de.baconsquirrel.hassiokt.connector.general.EntityEvent
-import de.baconsquirrel.hassiokt.connector.general.EntityState
-import de.baconsquirrel.hassiokt.connector.general.LightTurnOffMessage
-import de.baconsquirrel.hassiokt.connector.general.LightTurnOnMessage
-import de.baconsquirrel.hassiokt.connector.general.MediaPlayerPauseMessage
-import de.baconsquirrel.hassiokt.connector.general.MediaPlayerPlayMediaMessage
-import de.baconsquirrel.hassiokt.connector.general.MediaPlayerPlayMessage
-import de.baconsquirrel.hassiokt.connector.general.MediaPlayerStopMessage
-import de.baconsquirrel.hassiokt.connector.general.MediaPlayerTurnOffMessage
-import de.baconsquirrel.hassiokt.connector.general.RgbColor
-import de.baconsquirrel.hassiokt.connector.general.ServiceMessage
-import de.baconsquirrel.hassiokt.connector.general.SimpleEntityEvent
-import de.baconsquirrel.hassiokt.connector.general.SimpleEntityState
-import de.baconsquirrel.hassiokt.connector.general.SunEntityState
-import de.baconsquirrel.hassiokt.connector.general.SwitchTurnOffMessage
-import de.baconsquirrel.hassiokt.connector.general.SwitchTurnOnMessage
-import de.baconsquirrel.hassiokt.connector.general.TimerCancelMessage
-import de.baconsquirrel.hassiokt.connector.general.TimerStartMessage
+import de.baconsquirrel.hassiokt.connector.general.*
 
 internal fun JsonArray<JsonObject>.toEntityStateObjects(): List<EntityState> =
     this.mapNotNull { it.toEntityStateObject() }
@@ -41,7 +22,7 @@ internal fun JsonObject.toEntityStateObject(): EntityState? {
 internal fun JsonObject.toEntityEventObject(): EntityEvent? {
     return SimpleEntityEvent(
         id = this.string("id") ?: return null,
-        event = this.get("event")?.toString() ?: return null
+        event = this["event"]?.toString() ?: return null
     )
 }
 
@@ -59,15 +40,21 @@ internal fun ServiceMessage.toJsonCommandMessages(): Sequence<JsonObject> {
 
         is SwitchTurnOffMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is SwitchTurnOnMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is SwitchToggleMessage -> sequenceOf(baseObj + this.providePropertyAddition())
 
         is BooleanTurnOffMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is BooleanTurnOnMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is BooleanToggleMessage -> sequenceOf(baseObj + this.providePropertyAddition())
 
         is MediaPlayerTurnOffMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is MediaPlayerPlayMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is MediaPlayerPauseMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is MediaPlayerStopMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is MediaPlayerPlayMediaMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is MediaPlayerVolumeUpMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is MediaPlayerVolumeDownMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is MediaPlayerNextTrackMessage -> sequenceOf(baseObj + this.providePropertyAddition())
+        is MediaPlayerPreviousTrackMessage -> sequenceOf(baseObj + this.providePropertyAddition())
 
         is TimerStartMessage -> sequenceOf(baseObj + this.providePropertyAddition())
         is TimerCancelMessage -> sequenceOf(baseObj + this.providePropertyAddition())
@@ -83,9 +70,11 @@ private fun LightTurnOnMessage.providePropertyAdditions(): Sequence<JsonObject> 
 
 private fun SwitchTurnOffMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 private fun SwitchTurnOnMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
+private fun SwitchToggleMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 
 private fun BooleanTurnOffMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 private fun BooleanTurnOnMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
+private fun BooleanToggleMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 
 private fun MediaPlayerTurnOffMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 private fun MediaPlayerPlayMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
@@ -101,6 +90,14 @@ private fun MediaPlayerPlayMediaMessage.providePropertyAddition(): JsonObject =
             )
         )
     }
+
+private fun MediaPlayerVolumeUpMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
+private fun MediaPlayerVolumeDownMessage.providePropertyAddition(): JsonObject =
+    json { obj("service_data" to obj("entity_id" to entityId)) }
+
+private fun MediaPlayerNextTrackMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
+private fun MediaPlayerPreviousTrackMessage.providePropertyAddition(): JsonObject =
+    json { obj("service_data" to obj("entity_id" to entityId)) }
 
 private fun TimerStartMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
 private fun TimerCancelMessage.providePropertyAddition(): JsonObject = json { obj("service_data" to obj("entity_id" to entityId)) }
